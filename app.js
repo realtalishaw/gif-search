a95ef7685cc2f51f8c71ea55b48d3b1665abe5dc
Talisha <misstalishawhite@gmail.com>
2020-12-26 11:50:09 -0500

    added search form

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //Routes
+//example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifURL = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifURL into the view to be displayed
-    res.render('hello-gif', { gifURL })
+    res.render('home')
+    console.log(req.query) // => "{ term: hey }"
 });
 
 //Start Server
