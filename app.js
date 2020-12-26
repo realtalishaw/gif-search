5bce9f1b12389eb47f1a3db38746e351b7604258
Talisha <misstalishawhite@gmail.com>
2020-12-26 11:36:49 -0500

    handlebars are cool

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const express = require('express');
 const app = express();
 
 //Middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel!');
+    // set the url of the gif
+    const gifURL = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifURL into the view to be displayed
+    res.render('hello-gif', { gifURL })
 });
 
 //Start Server
